6cb9f79345e09ae4cdc7448799a9bed652fb4fe3
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 10:09:34 +0800

    Create TableList component.

---
+++ src/components/TableList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+const TableList = () => {
+  return (
+    <React.Fragment>
+      <Table striped bordered hover size="sm"></Table>
+    </React.Fragment>
+  );
+};
+
+export default TableList;
