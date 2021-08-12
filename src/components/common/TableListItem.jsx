48efcd3c3e6b58059778b9ae097836cdfab88e63
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 13:23:05 +0800

    Modify TableListItem layout.

--- src/components/common/TableListItem.jsx
+++ src/components/common/TableListItem.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
-const TableListItem = () => {
+const TableListItem = ({ item }) => {
+  const titleLinkStyle = {
+    color: "#fff",
+    textDecoration: "none",
+  };
+
+  const subtitleLinkStyle = {
+    textDecoration: "none",
+    fontSize: "13px",
+  };
+
+  const userLinkStyle = {
+    textDecoration: "none",
+    fontSize: "13px",
+  };
+
   return (
-    <div className="row">
-      <div className="col">Issue detail</div>
+    <div className="row mx-1">
+      <div className="col">
+        <div>
+          <a className="fw-bold" style={titleLinkStyle} href="/">
+            Compiled template classes preserve whitespace
+          </a>
+          <span className="mx-2 badge rounded-pill bg-warning text-dark">
+            feature request
+          </span>
+        </div>
+        <div>
+          <span className="text-muted" style={subtitleLinkStyle}>
+            #12143 opened on Jun 21 by{" "}
+            <a className="text-muted" style={userLinkStyle} href="/">
+              Juan dela Cruz
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
