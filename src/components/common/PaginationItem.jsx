f6714dd269371c13d60d41ac6e284e20b64a80bd
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 14:21:55 +0800

    Modify PaginationItem component layout.

--- src/components/common/PaginationItem.jsx
+++ src/components/common/PaginationItem.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const PaginationItem = () => {
-  return <React.Fragment></React.Fragment>;
+  const linkStyle = {
+    color: "#fff",
+    backgroundColor: "transparent",
+    borderColor: "transparent",
+  };
+
+  return (
+    <React.Fragment>
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" style={linkStyle} href="/">
+              Previous
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" style={linkStyle} href="/">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" style={linkStyle} href="/">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" style={linkStyle} href="/">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" style={linkStyle} href="/">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
 };
 
 export default PaginationItem;
