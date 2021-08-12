1638978a584247e253f0a4811c645173a1eea648
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 18:48:58 +0800

    Add global css styling for components.

--- src/components/common/TableListItem.jsx
+++ src/components/common/TableListItem.jsx
@@ -1,40 +1,23 @@
 import React from "react";
 
 const TableListItem = ({ item }) => {
-  const titleLinkStyle = {
-    color: "#fff",
-    textDecoration: "none",
-  };
-
-  const subtitleLinkStyle = {
-    textDecoration: "none",
-    fontSize: "13px",
-  };
-
-  const userLinkStyle = {
-    textDecoration: "none",
-    fontSize: "13px",
-  };
-
   return (
-    <div className="row mx-1">
-      <div className="col">
-        <div>
-          <a className="fw-bold" style={titleLinkStyle} href="/">
-            {item.title}
+    <div className="mx-2">
+      <div>
+        <a className="fw-bold text-decoration-none" href="/">
+          {item.title}
+        </a>
+        <span className="mx-2 badge rounded-pill bg-warning text-dark">
+          {item.type}
+        </span>
+      </div>
+      <div>
+        <span className="text-muted description">
+          #{item.id} opened on {item.date} by{" "}
+          <a className="text-muted text-decoration-none description" href="/">
+            {item.author}
           </a>
-          <span className="mx-2 badge rounded-pill bg-warning text-dark">
-            {item.type}
-          </span>
-        </div>
-        <div>
-          <span className="text-muted" style={subtitleLinkStyle}>
-            #{item.id} opened on {item.date} by{" "}
-            <a className="text-muted" style={userLinkStyle} href="/">
-              {item.author}
-            </a>
-          </span>
-        </div>
+        </span>
       </div>
     </div>
   );
