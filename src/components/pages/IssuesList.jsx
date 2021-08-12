00392d74642131d5e6290802bb90e6c45fcb8b29
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 13:57:26 +0800

    Set up dummy data for github issues.

--- src/components/pages/IssuesList.jsx
+++ src/components/pages/IssuesList.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import TableList from "../common/TableList";
-import PaginationItem from "../common/PaginationItem";
+import TextInput from "../common/TextInput";
+import RadioButtonGroup from "../common/RadioButtonGroup";
 
 const IssuesList = () => {
+  const [issues, setIssues] = useState([
+    {
+      id: 223,
+      title: "Sample Title 1",
+      description: "Sample description",
+      date: "2020-12-01",
+      author: "Juan dela cruz",
+      type: "feature request",
+    },
+    {
+      id: 32452,
+      title: "Sample Title 2",
+      description: "Sample description",
+      date: "2021-01-01",
+      author: "Juan dela cruz",
+      type: "feature request",
+    },
+    {
+      id: 4096,
+      title: "Sample Title 3",
+      description: "Sample description",
+      date: "2019-01-01",
+      author: "Maria dela cruz",
+      type: "feature request",
+    },
+  ]);
+
   return (
-    <div>
-      <TableList />
-      <PaginationItem />
+    <div className="container my-3">
+      <div className="card bg-dark text-white">
+        <div className="card-header">
+          <div className="row">
+            <div className="col">
+              <TextInput />
+            </div>
+            <div className="col-2 d-flex flex-row-reverse">
+              <RadioButtonGroup />
+            </div>
+          </div>
+        </div>
+        <TableList issues={issues} />
+      </div>
     </div>
   );
 };
