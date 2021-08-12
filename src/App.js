b75c9f78a2af4d2cef6dd3e8236f65c93b07e7f9
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 10:24:41 +0800

    Modify directories of each component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import IssuesList from "./components/pages/IssuesList";
 
 const App = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <IssuesList />
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/pages/IssuesList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TableList from "../common/TableList";
+import PaginationItem from "../common/PaginationItem";
+
+const IssuesList = () => {
+  return (
+    <div>
+      <TableList />
+      <PaginationItem />
+    </div>
+  );
+};
+
+export default IssuesList;
