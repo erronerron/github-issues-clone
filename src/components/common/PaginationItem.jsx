18cd7d8e4b74a328c46997557f2931949d162a41
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 13:54:40 +0800

    Modify PaginationItem component

--- src/components/common/PaginationItem.jsx
+++ src/components/common/PaginationItem.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { Pagination } from "react-bootstrap";
 
 const PaginationItem = () => {
-  return (
-    <React.Fragment>
-      <Pagination>
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis disabled />
-
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-
-        <Pagination.Ellipsis disabled />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next />
-      </Pagination>
-    </React.Fragment>
-  );
+  return <React.Fragment></React.Fragment>;
 };
 
 export default PaginationItem;
