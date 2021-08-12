7ad1c9853c075e20a7af67b9845c149caabcbfa6
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 14:22:19 +0800

    Modify TextInput component layout.

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,7 +1,11 @@
 const TextInput = () => {
   return (
     <div className="input-group input-group-sm">
-      <input type="text" className="form-control bg-dark"></input>
+      <input
+        type="text"
+        className="form-control bg-dark"
+        style={{ borderColor: "grey", color: "#fff" }}
+      ></input>
     </div>
   );
 };
