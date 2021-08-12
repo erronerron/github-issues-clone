9a56e73dfda75fe35b257637487f5b754fa5ee81
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 10:13:00 +0800

    Create TableListItem component.

---
+++ src/components/TableListItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableListItem = () => {
+  return (
+    <div className="row">
+      <div className="col">Issue detail</div>
+    </div>
+  );
+};
+
+export default TableListItem;
