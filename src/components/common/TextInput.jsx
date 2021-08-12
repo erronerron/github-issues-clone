33aaa13a12963128e87aef2c2e6bd4b68b2e40fa
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-12 14:00:48 +0800

    Create TextInput component for Github issues filtering.

---
+++ src/components/common/TextInput.jsx
@@ -0,0 +1,9 @@
+const TextInput = () => {
+  return (
+    <div className="input-group input-group-sm">
+      <input type="text" className="form-control bg-dark"></input>
+    </div>
+  );
+};
+
+export default TextInput;
