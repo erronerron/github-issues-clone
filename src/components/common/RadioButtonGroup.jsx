03457fbfc7c450bb046a1e511b3a9d50f1f7bb90
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-13 10:44:48 +0800

    Modify RadioButtonGroup component not triggering to HTML <input type="radio"> onChange event.

--- src/components/common/RadioButtonGroup.jsx
+++ src/components/common/RadioButtonGroup.jsx
@@ -1,36 +1,26 @@
-const RadioButtonGroup = () => {
+import React from "react";
+
+const RadioButtonGroup = ({ items, selected, onSelectItem }) => {
   return (
     <div className="btn-group" role="group">
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio1"
-        autoComplete="off"
-      />
-      <label className="btn btn-outline-secondary btn-sm" htmlFor="btnradio1">
-        All
-      </label>
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio2"
-        autoComplete="off"
-      />
-      <label className="btn btn-outline-secondary btn-sm" htmlFor="btnradio2">
-        Open
-      </label>
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio3"
-        autoComplete="off"
-      />
-      <label className="btn btn-outline-secondary btn-sm" htmlFor="btnradio3">
-        Closed
-      </label>
+      {items.map((item, index) => {
+        return (
+          <React.Fragment key={index}>
+            <button
+              type="button"
+              className={`btn btn-sm ${
+                item === selected
+                  ? "btn-primary disabled"
+                  : "btn-outline-secondary"
+              }`}
+              onClick={onSelectItem}
+              value={item}
+            >
+              {item}
+            </button>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
