65bc3727a2458f4cf3f374a48c2d24a8a375ba1c
Erron Intila <erron.intila@sun-asterisk.com>
2021-08-13 11:17:15 +0800

    Modify TableListItem component to match format of fetched values from github issues api.

--- src/components/common/TableListItem.jsx
+++ src/components/common/TableListItem.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 
 const TableListItem = ({ item }) => {
+  const badges = item.labels.map((item, index) => {
+    let badgeStyle = {
+      backgroundColor: `#${item.color}`,
+    };
+
+    return (
+      <span key={index} className="mx-2 badge rounded-pill" style={badgeStyle}>
+        {item.name}
+      </span>
+    );
+  });
+
+  const formattedDate = (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
   return (
     <div className="mx-2">
       <div>
-        <a className="fw-bold text-decoration-none" href="/">
+        <a className="fw-bold text-decoration-none" href={item.html_url}>
           {item.title}
         </a>
-        <span className="mx-2 badge rounded-pill bg-warning text-dark">
-          {item.type}
-        </span>
+        {badges}
       </div>
       <div>
         <span className="text-muted description">
-          #{item.id} opened on {item.date} by{" "}
-          <a className="text-muted text-decoration-none description" href="/">
-            {item.author}
+          #{item.id} opened on {formattedDate(item.updated_at)} by{" "}
+          <a
+            className="text-muted text-decoration-none description"
+            href={item.user.html_url}
+          >
+            {item.user.login}
           </a>
         </span>
       </div>
