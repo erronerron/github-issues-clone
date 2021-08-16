eff7594e18ad8712064f2df2ad3155a3f532677f
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-16 16:32:25 +0800

    Use history from react-router to update query parameters.

--- src/components/common/PaginationItem.jsx
+++ src/components/common/PaginationItem.jsx
@@ -1,35 +1,57 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 const PaginationItem = ({ page, lastPage, onPageChange }) => {
+  const history = useHistory();
 
   const handlePreviousPageClick = (e) => {
     page = parseInt(page);
     if (page <= 1) return;
     onPageChange(page - 1);
+
+    history.push({
+      pathname: '/',
+      search: `?page=${page - 1}`
+    });
   };
 
   const handleNextPageClick = (e) => {
     page = parseInt(page);
-    if(lastPage) return;
+    if (lastPage) return;
     onPageChange(page + 1);
+
+    history.push({
+      pathname: '/',
+      search: `?page=${page + 1}`
+    });
   };
+
+  
 
   return (
     <React.Fragment>
       <nav>
         <ul className="pagination">
           <li className="page-item">
-            <button 
+            <button
               className={page === 1 ? "btn btn-dark disabled" : "btn btn-dark"}
-              onClick={handlePreviousPageClick}>
-               Previous 
+              onClick={handlePreviousPageClick}
+            >
+              Previous
             </button>
           </li>
 
           <li className="page-item">
-            <button 
+            <button className="btn btn-primary readonly" value={page}>
+              {page}
+            </button>
+          </li>
+
+          <li className="page-item">
+            <button
               className={lastPage ? "btn btn-dark disabled" : "btn btn-dark"}
-              onClick={handleNextPageClick}>
+              onClick={handleNextPageClick}
+            >
               Next
             </button>
           </li>
@@ -37,6 +59,8 @@ const PaginationItem = ({ page, lastPage, onPageChange }) => {
       </nav>
     </React.Fragment>
   );
+
+  
 };
 
 export default PaginationItem;
