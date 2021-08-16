27d4a5aaa60cbfc80286196436399c8d5f3b652d
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-16 14:03:12 +0800

    Set up IssuesDetail page component.

---
+++ src/components/pages/IssuesDetail.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const IssuesDetail = () => {
+  return (
+    <div className="card bg-dark text-white">
+      <div className="card-header"></div>
+      <div className="card-footer d-flex justify-content-center"></div>
+    </div>
+  );
+};
+
+export default IssuesDetail;
