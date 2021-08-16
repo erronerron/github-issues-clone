19b02cc48171b7ca24e17fb93474a41b417c2a91
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-16 13:07:00 +0800

    Create layout for pagination and set up props.

--- src/components/common/PaginationItem.jsx
+++ src/components/common/PaginationItem.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-const PaginationItem = ({ page, itemsPerPage, totalItems, onPageChange }) => {
+const PaginationItem = ({ page, lastPage, onPageChange }) => {
+
   const handlePreviousPageClick = (e) => {
+    page = parseInt(page);
     if (page <= 1) return;
     onPageChange(page - 1);
   };
 
   const handleNextPageClick = (e) => {
-    if (page >= Math.ceil(totalItems / itemsPerPage)) return;
+    page = parseInt(page);
+    if(lastPage) return;
     onPageChange(page + 1);
-  };
-
-  const handlePageClick = (e) => {
-    if (e.target.value === page) return;
-    onPageChange(e.target.value);
   };
 
   return (
@@ -21,19 +19,17 @@ const PaginationItem = ({ page, itemsPerPage, totalItems, onPageChange }) => {
       <nav>
         <ul className="pagination">
           <li className="page-item">
-            <button className="page-link" onClick={handlePreviousPageClick}>
-              Previous
-            </button>
-          </li>
-
-          <li className="page-item active">
-            <button className="page-link" value="1" onClick={handlePageClick}>
-              1
+            <button 
+              className={page === 1 ? "btn btn-dark disabled" : "btn btn-dark"}
+              onClick={handlePreviousPageClick}>
+               Previous 
             </button>
           </li>
 
           <li className="page-item">
-            <button className="page-link" onClick={handleNextPageClick}>
+            <button 
+              className={lastPage ? "btn btn-dark disabled" : "btn btn-dark"}
+              onClick={handleNextPageClick}>
               Next
             </button>
           </li>
