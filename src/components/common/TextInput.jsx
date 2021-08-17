eb641c27945d613009786426bdbbf128af80529b
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-17 13:32:02 +0800

    Add TextInput component to handle search input.

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,10 +1,41 @@
-const TextInput = () => {
+import { useHistory } from "react-router";
+import queryString from "query-string";
+
+const TextInput = ({ onInputChange }) => {
+  const history = useHistory();
+  const parsed = queryString.parse(history.location.search);
+
+  const updateQueryParams = (newValue) => {
+    parsed.search = newValue;
+
+    history.push({
+      search: queryString.stringify(parsed),
+    });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onInputChange(e.target.value);
+      updateQueryParams(e.target.value);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.value === "") {
+      onInputChange(e.target.value);
+      updateQueryParams(e.target.value);
+      return;
+    }
+    return;
+  };
+
   return (
     <div className="input-group input-group-sm">
       <input
         type="text"
-        className="form-control bg-dark"
-        style={{ borderColor: "grey", color: "#fff" }}
+        className="form-control form-sm bg-dark text-input"
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
       ></input>
     </div>
   );
