2976a465da2c9f759f292e4b5f7d303190dd5767
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-18 16:02:57 +0800

    Set up store.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import PageHeader from "./components/common/PageHeader";
 import IssuesList from "./components/pages/IssuesList";
 
