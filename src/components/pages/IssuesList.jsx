614d74814ea1b1176136100d0fd843111d53b277
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-19 10:32:47 +0800

    Modify redux implementation based on hooks.

--- src/components/pages/IssuesList.jsx
+++ src/components/pages/IssuesList.jsx
@@ -1,55 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { fetchIssues } from "../../store/actions";
-
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
-import github from "../../api/github";
+
+import { fetchIssues } from "../../store/actions";
 import TableList from "../common/TableList";
 import PaginationItem from "../common/PaginationItem";
 import RadioButtonGroup from "../common/RadioButtonGroup";
 import SearchForm from "../common/SearchForm";
 
 const IssuesList = (props) => {
+  const issues = useSelector((state) => state.issues);
+  const dispatch = useDispatch();
+
   // Query Parameters
   const { search } = useLocation();
   const parsed = queryString.parse(search);
 
+  // Repository Information
+  const [owner, setOwner] = useState("");
+  const [repository, setRepository] = useState("");
+
   // Status filter
-  const [status, setStatus] = useState(parsed.status ?? "Open");
-  const statuses = ["All", "Open", "Closed"];
+  const [status, setStatus] = useState(parsed.status ?? "open");
+  const statuses = ["all", "open", "closed"];
 
   // Pagination
   const [page, setPage] = useState(parsed.page ?? 1);
-  const [lastPage, setLastPage] = useState(false);
   const itemsPerPage = 10;
 
   useEffect(() => {
-    const getData = async () => {
-      // try {
-      //   const response = await github.get("repos/mui-org/material-ui/issues", {
-      //     params: {
-      //       page: page,
-      //       per_page: itemsPerPage,
-      //       state: status.toLowerCase(),
-      //     },
-      //   });
-      //   setLastPage(response.data.length === 0 || response.data.length < 10);
-      //   setIssues(response.data ?? []);
-      // } catch (error) {
-      //   console.log(error.response);
-      // }
-    };
-
-    getData();
-  }, [page, status]);
+    dispatch(fetchIssues(owner, repository, page, itemsPerPage, status));
+  }, [dispatch, owner, repository, page, status]);
 
   const handleSelectStatus = (status) => {
+    setPage(1);
     setStatus(status);
   };
 
   const handlePageChange = (pageNum) => {
     setPage(pageNum);
+  };
+
+  const handleFormSubmit = (formValues) => {
+    setOwner(formValues.owner);
+    setRepository(formValues.repository);
   };
 
   return (
@@ -61,97 +56,25 @@ const IssuesList = (props) => {
             selected={status}
             onSelectItem={handleSelectStatus}
           />
-          <SearchForm />
+          <SearchForm onFormSubmit={handleFormSubmit} />
         </div>
 
-        <TableList issues={props.issues} />
+        {issues.length ? (
+          <TableList issues={issues} />
+        ) : (
+          <div className="text-center">No records found.</div>
+        )}
 
         <div className="card-footer d-flex justify-content-center">
-          <PaginationItem
-            page={page}
-            lastPage={lastPage}
-            onPageChange={handlePageChange}
-          />
+          {issues.length ? (
+            <PaginationItem page={page} onPageChange={handlePageChange} />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return { issues: Object.values(state.issues) };
-};
-
-// const IssuesList = () => {
-//   // Query Parameters
-//   const { search } = useLocation();
-//   const parsed = queryString.parse(search);
-
-//   // List of issues
-//   const [issues, setIssues] = useState([]);
-
-//   // Status filter
-//   const [status, setStatus] = useState(parsed.status ?? "Open");
-//   const statuses = ["All", "Open", "Closed"];
-
-//   // Pagination
-//   const [page, setPage] = useState(parsed.page ?? 1);
-//   const [lastPage, setLastPage] = useState(false);
-//   const itemsPerPage = 10;
-
-//   useEffect(() => {
-//     const getData = async () => {
-//       try {
-//         const response = await github.get("repos/mui-org/material-ui/issues", {
-//           params: {
-//             page: page,
-//             per_page: itemsPerPage,
-//             state: status.toLowerCase(),
-//           },
-//         });
-
-//         setLastPage(response.data.length === 0 || response.data.length < 10);
-//         setIssues(response.data ?? []);
-//       } catch (error) {
-//         console.log(error.response);
-//       }
-//     };
-
-//     getData();
-//   }, [page, status]);
-
-//   const handleSelectStatus = (status) => {
-//     setStatus(status);
-//   };
-
-//   const handlePageChange = (pageNum) => {
-//     setPage(pageNum);
-//   };
-
-//   return (
-//     <div className="container my-3">
-//       <div className="card bg-dark text-white card-prop">
-//         <div className="card-header d-flex justify-content-between">
-//           <RadioButtonGroup
-//             items={statuses}
-//             selected={status}
-//             onSelectItem={handleSelectStatus}
-//           />
-//           <SearchForm />
-//         </div>
-
-//         <TableList issues={issues} />
-
-//         <div className="card-footer d-flex justify-content-center">
-//           <PaginationItem
-//             page={page}
-//             lastPage={lastPage}
-//             onPageChange={handlePageChange}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-export default connect(mapStateToProps, { fetchIssues })(IssuesList);
+export default IssuesList;
