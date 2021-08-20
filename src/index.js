482aea631b7e2f2c9a1c7c2b12d88d2a1bf749fa
Erron Intila <88467532+erronerron@users.noreply.github.com>
2021-08-20 14:03:44 +0800

    Install dependencies and import files.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "github-markdown-css";
 import "./index.css";
-import App from './App';
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import store from './store/store';
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
